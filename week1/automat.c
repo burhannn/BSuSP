3d112fbe05059a144dc4eebec42938a5ec27782a
Burhan <35541515+burhannn@users.noreply.github.com>
2020-11-12 16:19:02 +0100

    Update automat.c

--- week1/automat.c
+++ week1/automat.c
@@ -6,13 +6,7 @@
 //
 
 #include "automat.h"
-
-int calc_coins(int coin, int amount){
-    int n = amount/coin;
-    printf("Anzahl der %2i-Cent Muenzen: %3i\n", coin, n);
-    amount = amount - n*coin;
-    return amount;
-}
+#include "calc.h"
 
 int main(){
     int amount;
